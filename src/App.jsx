3a354bf8e3d32c99703ef07ecdc90d00f1d77e1e
emma-landry7 <emmablandry730@gmail.com>
2023-12-16 16:02:23 -0600

    completed Module 6 Lab 1: Greeting Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Greeting from '../components/Greeting'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -24,10 +25,13 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Greeting name="John">
+        <div>This is a child component</div>
+      </Greeting>
     </>
   )
 }
