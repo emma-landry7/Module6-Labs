ff70bece224e60e27d99c39507d2c16f62343119
emma-landry7 <emmablandry730@gmail.com>
2023-12-28 23:30:09 -0600

    added button

--- components/SingleCat.jsx
+++ components/SingleCat.jsx
@@ -3,6 +3,7 @@ function SingleCat({image, name, latinName}) {
         <li style={{textAlign: "left"}}>
             <img width="400px" style={{marginRight: 8}} src={image} alt={`image of ${name}`} />
             <span><strong>{name}:</strong> {latinName}</span>
+            <span><button>Delete Cat</button></span>
         </li>
     )
 }
